bd5a7fa8263d43d1641d4ad109965b833872a891
MathBenDAI <cfdtlee@gmail.com>
2015-12-16 22:02:00 -0500

    start week 2

---
+++ GrammyPlus/GrammyPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GrammyPlus
+//
+//  Created by Eric on 12/16/15.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GrammyPlus/GrammyPlus/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GrammyPlus
+//
+//  Created by Eric on 12/16/15.
+//  Copyright © 2015 Eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
